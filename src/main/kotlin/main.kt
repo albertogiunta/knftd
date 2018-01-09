7e8c723b0e9f91ab210a964014709cb4091d14f4
albertogiunta <alb.giunta@gmail.com>
2018-01-09 10:26:11 +0100

    REFACTOR code for main algorithm ADDED actual food images

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,50 @@
+import CANVAS.canvasFrame
 import FileUtils.getImg
-import org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U
-import org.bytedeco.javacpp.opencv_core.Mat
+import IMG.imgConverter
+import IMG.originalImg
+import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_UNCHANGED
 import org.bytedeco.javacpp.opencv_imgcodecs.imread
 import org.bytedeco.javacpp.opencv_imgproc.*
 import org.bytedeco.javacv.CanvasFrame
 import org.bytedeco.javacv.OpenCVFrameConverter
 
-fun main(args: Array<String>) {
-    cannyWithMatLoader()
+object IMG {
+    val originalImg: Mat = imread(getImg("biscotti.jpg"), CV_LOAD_IMAGE_UNCHANGED).also { resizeSelf(it) }
+    val imgConverter = OpenCVFrameConverter.ToMat()
 }
 
-fun cannyWithMatLoader() {
-    val threshold = 20.0
-    val apertureSize = 3
-    val converter = OpenCVFrameConverter.ToMat()
-    val canvasFrame = CanvasFrame("Canny filter")
-    val originalImg = imread(getImg("test2.png"), CV_LOAD_IMAGE_UNCHANGED)
-    with(canvasFrame) {
+object CANVAS {
+    val canvasFrame = CanvasFrame("KNFTD - Kotlin Nutritional Facts Table Detector").apply {
         isResizable = false
         setCanvasSize(originalImg.size().width(), originalImg.size().height())
     }
-
-    val modifiedImg = Mat(originalImg.size().width(), originalImg.size().height(), IPL_DEPTH_8U, 1)
-    cvtColor(originalImg, modifiedImg, CV_BGR2GRAY)
-    Canny(modifiedImg, modifiedImg, threshold, (threshold * 1), apertureSize, true)
-
-    canvasFrame.showImage(converter.convert(modifiedImg))
-
 }
+
+object CANNY {
+    val threshold = 20.0
+    val apertureSize = 3
+}
+
+fun main(args: Array<String>) {
+
+    // copy original     image into a new one to be used for filter applying
+    val modifiedImg = cloneImageFrom(originalImg)
+
+    // 1) convert to greyscale
+    cvtColor(originalImg, modifiedImg, CV_BGR2GRAY)
+
+    // 2) apply canny edge detection
+    Canny(modifiedImg, modifiedImg, CANNY.threshold, (CANNY.threshold * 1), CANNY.apertureSize, true)
+
+    // 3) apply Hough transform
+
+    // 4) correct rotation w/ average horizontal 0
+
+    // final) show image to screen
+    canvasFrame.showImage(imgConverter.convert(modifiedImg))
+}
+
+fun resizeSelf(img: Mat) = resize(img, img, Size((img.size().width() * 0.2).toInt(), (img.size().height() * 0.2).toInt()))
+
+fun cloneImageFrom(img: Mat) = Mat(img.size().width(), img.size().height(), IPL_DEPTH_8U, 1)
