49da6f1c86d47d48180b78505f855e6ecc973f22
albertogiunta <alb.giunta@gmail.com>
2018-01-09 10:48:13 +0100

    REFACTOR code for main algorithm

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,12 +2,14 @@ import CANVAS.canvasFrame
 import FileUtils.getImg
 import IMG.imgConverter
 import IMG.originalImg
+import net.sourceforge.tess4j.Tesseract
 import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_UNCHANGED
 import org.bytedeco.javacpp.opencv_imgcodecs.imread
 import org.bytedeco.javacpp.opencv_imgproc.*
 import org.bytedeco.javacv.CanvasFrame
 import org.bytedeco.javacv.OpenCVFrameConverter
+import java.io.File
 
 object IMG {
     val originalImg: Mat = imread(getImg("biscotti.jpg"), CV_LOAD_IMAGE_UNCHANGED).also { resizeSelf(it) }
@@ -27,7 +29,10 @@ object CANNY {
 }
 
 fun main(args: Array<String>) {
+    runMainAlgorithm()
+}
 
+fun runMainAlgorithm() {
     // copy original     image into a new one to be used for filter applying
     val modifiedImg = cloneImageFrom(originalImg)
 
@@ -43,6 +48,14 @@ fun main(args: Array<String>) {
 
     // final) show image to screen
     canvasFrame.showImage(imgConverter.convert(modifiedImg))
+}
+
+fun ocr() {
+    val tess = Tesseract()
+    tess.setLanguage("ita")
+    val image = File(getImg("biscotti.jpg"))
+    val r = tess.doOCR(image)
+    println(r)
 }
 
 fun resizeSelf(img: Mat) = resize(img, img, Size((img.size().width() * 0.2).toInt(), (img.size().height() * 0.2).toInt()))
