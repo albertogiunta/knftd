c20aa3ceb91b6619a3a69d0b5208b310f3d0315d
albertogiunta <alb.giunta@gmail.com>
2018-01-09 13:08:15 +0100

    ADDED Hough transform

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,6 +3,8 @@ import FileUtils.getImg
 import IMG.imgConverter
 import IMG.originalImg
 import net.sourceforge.tess4j.Tesseract
+import org.bytedeco.javacpp.DoublePointer
+import org.bytedeco.javacpp.indexer.FloatRawIndexer
 import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_UNCHANGED
 import org.bytedeco.javacpp.opencv_imgcodecs.imread
@@ -10,9 +12,11 @@ import org.bytedeco.javacpp.opencv_imgproc.*
 import org.bytedeco.javacv.CanvasFrame
 import org.bytedeco.javacv.OpenCVFrameConverter
 import java.io.File
+import java.lang.Math.*
+
 
 object IMG {
-    val originalImg: Mat = imread(getImg("biscotti.jpg"), CV_LOAD_IMAGE_UNCHANGED).also { resizeSelf(it) }
+    val originalImg: Mat = imread(getImg("dado.jpg"), CV_LOAD_IMAGE_UNCHANGED).also { resizeSelf(it) }
     val imgConverter = OpenCVFrameConverter.ToMat()
 }
 
@@ -24,7 +28,7 @@ object CANVAS {
 }
 
 object CANNY {
-    val threshold = 20.0
+    val threshold = 40.0
     val apertureSize = 3
 }
 
@@ -43,11 +47,66 @@ fun runMainAlgorithm() {
     Canny(modifiedImg, modifiedImg, CANNY.threshold, (CANNY.threshold * 1), CANNY.apertureSize, true)
 
     // 3) apply Hough transform
+    val distanceResolutionInPixels = 0.3
+    val angleResolutionInRadians = 0.015
+    val minimumVotes = 150
+
+    val lines = Mat()
+
+    HoughLines(
+            modifiedImg,
+            lines,
+            distanceResolutionInPixels,
+            angleResolutionInRadians,
+            minimumVotes)
+
+    val indexer = lines.createIndexer() as FloatRawIndexer
+    val result = Mat().also { modifiedImg.copyTo(it) }
+
+    for (i in 0 until lines.rows()) {
+        val rho = indexer.get(i.toLong(), 0, 0)
+        val theta = indexer.get(i.toLong(), 0, 1).toDouble()
+
+        val pair = if (theta < PI / 4.0 || theta > 3.0 * PI / 4.0) {
+            // ~vertical line
+            // point of intersection of the line with first row
+            val p1 = Point(round(rho / cos(theta)).toInt(), 0)
+            // point of intersection of the line with last row
+            val p2 = Point(round((rho - result.rows() * sin(theta)) / cos(theta)).toInt(), result.rows())
+            Pair(p1, p2)
+        } else {
+            // ~horizontal line
+            // point of intersection of the line with first column
+            val p1 = Point(0, round(rho / sin(theta)).toInt())
+            // point of intersection of the line with last column
+            val p2 = Point(result.cols(), round((rho - result.cols() * cos(theta)) / sin(theta)).toInt())
+            Pair(p1, p2)
+        }
+
+        // draw a white line
+        line(result, pair.first, pair.second, Scalar(255.0, 0.0, 255.0, 0.0), 1, LINE_8, 0)
+    }
 
     // 4) correct rotation w/ average horizontal 0
 
     // final) show image to screen
-    canvasFrame.showImage(imgConverter.convert(modifiedImg))
+    canvasFrame.showImage(imgConverter.convert(toMat8U(result)))
+}
+
+fun toMat8U(src: Mat, doScaling: Boolean = true): Mat {
+    val minVal = DoublePointer(Double.MAX_VALUE)
+    val maxVal = DoublePointer(Double.MIN_VALUE)
+    minMaxLoc(src, minVal, maxVal, null, null, Mat())
+    val min = minVal.get(0)
+    val max = maxVal.get(0)
+    val (scale, offset) = if (doScaling) {
+        val s = 255.toDouble() / (max - min)
+        Pair(s, -min * s)
+    } else Pair(1.toDouble(), 0.0)
+
+    val dest = Mat()
+    src.convertTo(dest, CV_8U, scale, offset)
+    return dest
 }
 
 fun ocr() {
