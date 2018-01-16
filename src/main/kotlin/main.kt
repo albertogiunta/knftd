39764fa9583ce0394c18c389c72e9b9627c81424
albertogiunta <alb.giunta@gmail.com>
2018-01-16 15:16:53 +0100

    la lisa mi ha detto pusha, e io pusho

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,14 @@
+import CANNY.binaryThreshold
+import DICT.alignedYMargin
 import DICT.dictionaryProperties
 import DICT.dictionaryX
 import DICT.dictionaryY
 import DICT.distanceThresh
 import DICT.leven
+import DICT.lineMergingYDistance
+import DICT.lineMergingYDistanceForValuesAndPossibleMUOnNextLine
+import DICT.muSet
+import DICT.numberOfRowsToAddToTheActualNumberOfRows
 import DICT.shrinkedList
 import DICT.words
 import FileUtils.getImg
@@ -32,18 +38,23 @@ import java.lang.Math.*
 object DICT {
     val dictionaryProperties = listOf("energia", "energetico", "grassi", "acidi", "saturi", "insaturi", "monoinsaturi", "polinsaturi", "carboidrati", "zuccheri", "proteine", "fibre", "sale", "sodio", "fibre", "fibra", "alimentare", "amido")
     val dictionaryY = listOf("valori", "informazioni", "tabella", "dichiarazione", "nutrizionale", "nutrizionali")
-    val dictionaryX = listOf("grassi", "carboidrati", "proteine", "sale", "sodio")
+    val dictionaryX = listOf("grassi", "carboidrati", "proteine", "sale")
     val words = mutableListOf<Word>()
     val leven = NormalizedLevenshtein()
-    val distanceThresh = 0.5
     val shrinkedList = mutableListOf<Word>()
+    val distanceThresh = 0.5
+    val lineMergingYDistance = 5
+    val lineMergingYDistanceForValuesAndPossibleMUOnNextLine = 5
+    val numberOfRowsToAddToTheActualNumberOfRows = 10
+    val alignedYMargin = 20
+    val muSet = setOf("9", "g", "kcal", "kJ")
 }
 
 object IMG {
-    val imgName = "olioantiorario" + "." + "jpg"
-    val resizeRatio = 0.6
+    val imgName = "libere" + "/" + "olio3" + "." + "jpg"
+    val resizeRatio = 0.5
     val originalImgNotResized: Mat = imread(getImg(imgName), CV_LOAD_IMAGE_UNCHANGED)
-    val originalImg: Mat = imread(getImg(imgName), CV_LOAD_IMAGE_UNCHANGED).also { resizeSelf(it) }
+    val originalImg: Mat = imread(getImg(imgName), CV_LOAD_IMAGE_UNCHANGED).also { it.resizeSelf() }
     val imgConverter = OpenCVFrameConverter.ToMat()
     var properties = mutableListOf<CustomDistance>()
 }
@@ -51,6 +62,7 @@ object IMG {
 object CANNY {
     val threshold = 70.0
     val apertureSize = 3
+    val binaryThreshold = 50.0
 }
 
 object HOUGH {
@@ -74,7 +86,7 @@ fun main(args: Array<String>) {
 }
 
 fun runMainAlgorithm() {
-    // copy original     image into a new one to be used for filter applying
+    // copy original image into a new one to be used for filter applying
     var imgForHough = originalImg.clone()
 
     // 1) convert to greyscale
@@ -102,6 +114,8 @@ fun runMainAlgorithm() {
     // 2) apply otsu binary filter
     applyOtsu(imgForOCR)
 
+    imgForOCR.show("otsu")
+
     val rect = getRectForCrop(imgForOCR)
 
     val croppedOriginalNotResized = crop(originalImgNotResized, rect)
@@ -109,7 +123,7 @@ fun runMainAlgorithm() {
     // NB now original image is cropped to be only the table
 
     val imgForHough2 = croppedOriginalNotResized.clone()
-    resizeSelf(imgForHough2)
+    imgForHough2.resizeSelf()
 
     // 1) convert to greyscale
     convertToGreyscale(imgForHough2)
@@ -124,6 +138,8 @@ fun runMainAlgorithm() {
     // 4) correct rotation w/ average horizontal 0
     croppedOriginalNotResized.rotateToTheta()
 
+    croppedOriginalNotResized.clone().resizeSelf().show("Final rotation (resized and cropped)")
+
     val imgForOCR2 = croppedOriginalNotResized.clone()
     // 1) convert to greyscale
     convertToGreyscale(imgForOCR2)
@@ -132,6 +148,8 @@ fun runMainAlgorithm() {
 
     // 2) apply otsu binary filter
     applyBinary(imgForOCR2)
+
+    imgForOCR2.clone().resizeSelf().show("After binarization (non otsu)")
 
     words.clear()
     words.addAll(imgForOCR2.getWords())
@@ -143,7 +161,10 @@ fun runMainAlgorithm() {
     mergePropertiesWithValues()
 }
 
-fun resizeSelf(img: Mat) = resize(img, img, Size((img.size().width() * resizeRatio).toInt(), (img.size().height() * resizeRatio).toInt()))
+fun Mat.resizeSelf(): Mat {
+    resize(this, this, Size((this.size().width() * resizeRatio).toInt(), (this.size().height() * resizeRatio).toInt()))
+    return this
+}
 
 fun convertToGreyscale(source: Mat, dest: Mat = source) = cvtColor(source, dest, CV_BGR2GRAY)
 
@@ -158,7 +179,8 @@ fun Mat.rotateToTheta() {
 }
 
 fun applyOtsu(source: Mat, dest: Mat = source) = threshold(source, dest, 0.0, 255.0, THRESH_OTSU)
-fun applyBinary(source: Mat, dest: Mat = source) = threshold(source, dest, 50.0, 255.0, THRESH_BINARY)
+
+fun applyBinary(source: Mat, dest: Mat = source) = threshold(source, dest, binaryThreshold, 255.0, THRESH_BINARY)
 
 fun crop(source: Mat, rect: Rect): Mat = Mat(source, rect)
 
@@ -213,14 +235,10 @@ fun applyHough(source: Mat) {
         }
     }
 
-//    println("Result $houghCounter, NON RIMOSSE ${horizontalLinesList.size} ${verticalLinesList.size}")
     removeLines(horizontalLinesList)
     removeLines(verticalLinesList)
-//    println("Result $houghCounter, RIMOSSE ${horizontalLinesList.size} ${verticalLinesList.size}")
 
     finalTheta = if (verticalLinesList.isNotEmpty()) verticalLinesList.map { it.theta.toDegrees() }.average() else 0.0
-//    println("mean theta rad " + finalTheta)
-//    println("mean theta deg " + finalTheta * 180 / PI)
 
     val res2 = Mat().also { originalImg.copyTo(it) }
     //draw lines
@@ -229,7 +247,6 @@ fun applyHough(source: Mat) {
         if (it.theta == 0.0) line(res2, it.p1, it.p2, scalar2, 1, LINE_8, 0)
         else line(res2, it.p1, it.p2, scalar, 1, LINE_8, 0)
     }
-//    res2.show("HOUGH (lines removed) $houghCounter")
 
     houghCounter = houghCounter + 1
 }
@@ -240,6 +257,7 @@ fun getRectForCrop(source: Mat): Rect {
     words.addAll(source.getWords())
 
     val cropOffset = 0
+    //Computes the levenstein distance for each word with selected dictionaries which contain the keywords for cropping
     val wordY = words.map { word -> dictionaryY.map { dictWord -> CustomDistance(word, dictWord, leven.distance(word.text.toLowerCase(), dictWord)) }.minBy { it.distance } }.minBy { it!!.distance }!!.ocrWord
     val wordX = words.map { word -> dictionaryX.map { dictWord -> CustomDistance(word, dictWord, leven.distance(word.text.toLowerCase(), dictWord)) }.minBy { it.distance } }.minBy { it!!.distance }!!.ocrWord
     val x = wordX.boundingBox.x - cropOffset
@@ -250,8 +268,7 @@ fun getRectForCrop(source: Mat): Rect {
 
 fun extractNutritionalPropertyNames() {
 
-//    words.forEach { println(it) }
-
+    //Foreach word found by tesseract, it saves in a different structure only the words that are equal or similar to the ones in the dictionary
     words
             .forEach { ocrWord ->
                 dictionaryProperties
@@ -260,8 +277,12 @@ fun extractNutritionalPropertyNames() {
                         .forEach { properties.add(it) }
             }
 
+    //Contains indexes of words that have already been merged with a previous word
     val alreadyMergedPropertiesIndexes = mutableListOf<Int>()
+    //Contains merged proprieties (like "acidi grassi saturi")
     val newProperties = mutableListOf<CustomDistance>()
+
+    //Filters the words that match with more than one dictionary word, keeping only the one that matches more (has the smallest distance)
     properties = properties
             .map { main ->
                 properties
@@ -272,11 +293,14 @@ fun extractNutritionalPropertyNames() {
 
 
 
-    properties.forEachIndexed { i, customDistance ->
+    properties.forEachIndexed { i, _ ->
+        //If the word has not been merged already
         if (!alreadyMergedPropertiesIndexes.contains(i)) {
+            //Save the Word representing the property
             newProperties.add(properties[i])
             var j = i + 1
-            while (j < properties.size - 1 && Math.abs(properties[i].ocrWord.boundingBox.y - properties[j].ocrWord.boundingBox.y) < 5) {
+            //Check next Words, if they have almost the same Y values it means that they are on the same line, and it add the text to the current Word text
+            while (j < properties.size - 1 && Math.abs(properties[i].ocrWord.boundingBox.y - properties[j].ocrWord.boundingBox.y) < lineMergingYDistance) {
                 newProperties[newProperties.size - 1] = CustomDistance(properties[j].ocrWord, newProperties.last().dictWord + " " + properties[j].dictWord, newProperties.last().distance)
                 alreadyMergedPropertiesIndexes.add(j)
                 j++
@@ -291,19 +315,19 @@ fun extractNutritionalPropertyNames() {
 fun extractNutritionalPropertiesValues() {
 
     val maxXX = properties.maxBy { it.ocrWord.boundingBox.x + it.ocrWord.boundingBox.width }!!
+    //X value from which starting to look for nutritional values
     val maxX = maxXX.ocrWord.boundingBox.x + maxXX.ocrWord.boundingBox.width
-    val numRows = properties.mapIndexed { i, p -> if (i + 1 < properties.size) Math.abs(p.ocrWord.boundingBox.y - properties[i + 1].ocrWord.boundingBox.y) else 0 }.filter { it > 5 }.count() + 1
+    // probably there's no need for this one because properties are already shrinked in lines before
+    //val numRows = properties.mapIndexed { i, p -> if (i + 1 < properties.size) Math.abs(p.ocrWord.boundingBox.y - properties[i + 1].ocrWord.boundingBox.y) else 0 }.filter { it > 5 }.count() + 1
+    val numRows = properties.size
 
-    // valori più a sinistra dei nomi delle proprietà nutrizionali
-    val allRightValues = words.filter { it.boundingBox.x > maxX }.sortedBy { it.boundingBox.x }.take(numRows + 10).sortedBy { it.boundingBox.y }
+    // Selects only the Words that have the smallest X values (as many as the number of proprieties (plus a margin of error)
+    val allRightValues = words.filter { it.boundingBox.x > maxX }.sortedBy { it.boundingBox.x }.take(numRows + numberOfRowsToAddToTheActualNumberOfRows).sortedBy { it.boundingBox.y }
 
-//    allRightValues.sortedBy { it.boundingBox.y }.forEach { println(it.text) }
-
-    val muSet = setOf("9", "g", "kcal", "kJ")
-
+    //Fixes the error made with measurement units (often alone in one line, often a "9", etc) checking for each Word if the next one is a 9 and merging
     allRightValues.mapIndexed { i, p ->
         if (i + 1 < allRightValues.size) {
-            if ((muSet.contains(allRightValues[i + 1].text)) && Math.abs(p.boundingBox.y - allRightValues[i + 1].boundingBox.y) < 20) {
+            if ((muSet.contains(allRightValues[i + 1].text)) && Math.abs(p.boundingBox.y - allRightValues[i + 1].boundingBox.y) < lineMergingYDistanceForValuesAndPossibleMUOnNextLine) {
                 shrinkedList.add(Word(p.text + " g", p.confidence, p.boundingBox))
             } else if (!muSet.contains(p.text)) {
                 shrinkedList.add(Word(p.text, p.confidence, p.boundingBox))
@@ -318,11 +342,12 @@ fun mergePropertiesWithValues() {
 
     val map = mutableMapOf<CustomDistance, Word>()
 
+    //For each property looks for a corresponding value almost on the same Y value (aligned)
     properties.forEach { prop ->
         shrinkedList
                 .forEach { value ->
                     println("${prop.dictWord} - ${value.text} |  prop y ${prop.ocrWord.boundingBox.y} | value y ${value.boundingBox.y}")
-                    if (Math.abs(value.boundingBox.y - prop.ocrWord.boundingBox.y) < 20 && (!map.containsKey(prop) || value.boundingBox.x < map[prop]!!.boundingBox.x)) {
+                    if (Math.abs(value.boundingBox.y - prop.ocrWord.boundingBox.y) < alignedYMargin && (!map.containsKey(prop) || value.boundingBox.x < map[prop]!!.boundingBox.x)) {
                         map.put(prop, value)
                     }
                 }
