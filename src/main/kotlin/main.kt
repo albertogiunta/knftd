fbe4ea4fb52bbbe0a1013d8f842a96b301741365
lisamazzini <lisa.mazzini5@gmail.com>
2018-02-01 17:12:53 +0100

    odio la grafica odio javafx odio tutto

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,22 @@
+import javafx.application.Application
+import javafx.stage.FileChooser
+import javafx.stage.Stage
+import tornadofx.App
+
+
+class MainApp : App() {
+
+    override fun start(stage: Stage) {
+        val file = FileChooser().showOpenDialog(stage)
+
+        val imgName = file.absolutePath
+
+        val algorithm = MainAlgorithm(imgName)
+
+        algorithm.run()
+    }
+}
+
 fun main(args: Array<String>) {
-
-    val imgName = "nerosubianco" + "/" + "!test" + "/" + "zuppa" + "." + "jpg"
-
-    val algorithm = MainAlgorithm(imgName)
-
-    algorithm.run()
-
+    Application.launch(MainApp::class.java, *args)
 }
--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -26,7 +26,7 @@ object FileUtils {
 
     fun getRes() = "${System.getProperty("user.dir")}/res/"
 
-    fun getImg(imgName: String) = "${getRes()}$imgName"
+    fun getImg(imgName: String) = imgName
 
     fun getImgIgnorante(imgName: String) = "${System.getProperty("user.dir")}/$imgName"
 
